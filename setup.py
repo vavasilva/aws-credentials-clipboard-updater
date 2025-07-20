b642dd28493aee7046508bdb79812e4114f4c1ea
Wagner Silva <wra.silva@gmail.com>
2025-07-20 12:05:44 -0300

    feat(setup.py): bump version from 1.0.2 to 1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awscreds',
-    version='1.0.2',
+    version='1.0.5',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
