c41472d8b18e710e5840a6f391b98d461b34fe87
Wagner Silva <wra.silva@gmail.com>
2026-01-02 12:22:28 -0300

    build: update wheel to 4919 bytes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awscreds',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
